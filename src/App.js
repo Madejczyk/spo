dad1e7f02b99c482af1b34793c25fcc92c2b0c56
= <jakub.madejczyk92@gmail.com>
2017-10-05 23:34:35 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import Root from './components/layout/Root'
+
+render(<Root/>, window.document.getElementById("root"))
---
+++ src/components/layout/Root.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import {Login} from './'
+
+const Root = () => (
+    <Login/>
+)
+
+export default Root
